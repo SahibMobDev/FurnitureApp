1228f4419cd8b32299d8769ae2c143befd0f652e
SahibMobDev <sahibmobdev@gmail.com>
2023-07-05 08:24:23 +0400

    Project setup add dependencies

---
+++ app/src/main/java/com/example/furnitureapp/activities/LoginRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.furnitureapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.furnitureapp.R
+
+class LoginRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+}
--- app/src/main/java/com/example/furnitureapp/activities/ShoppingActivity.kt
+++ app/src/main/java/com/example/furnitureapp/activities/ShoppingActivity.kt
@@ -1,11 +1,12 @@
-package com.example.furnitureapp
+package com.example.furnitureapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.furnitureapp.R
 
-class MainActivity : AppCompatActivity() {
+class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_shopping)
     }
 }
