37352b69911879861a48d32bf656706084b17fb3
SahibMobDev <sahibmobdev@gmail.com>
2023-07-10 11:01:06 +0400

    Create accounts with email.

--- app/src/main/java/com/example/furnitureapp/fragments/loginRegister/RegisterFragment.kt
+++ app/src/main/java/com/example/furnitureapp/fragments/loginRegister/RegisterFragment.kt
@@ -1,7 +1,76 @@
 package com.example.furnitureapp.fragments.loginRegister
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.furnitureapp.R
+import com.example.furnitureapp.data.User
+import com.example.furnitureapp.databinding.FragmentRegisterBinding
+import com.example.furnitureapp.util.Resource
+import com.example.furnitureapp.viewmodel.RegisterViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+private const val TAG = "RegisterFragment"
+@AndroidEntryPoint
 class RegisterFragment : Fragment(R.layout.fragment_register) {
+
+    private lateinit var binding: FragmentRegisterBinding
+    private val viewModel by viewModels<RegisterViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            buttonRegisterRegister.setOnClickListener {
+                val user = User(
+                    edFirstNameRegister.text.toString().trim(),
+                    edLastNameRegister.text.toString().trim(),
+                    edEmailRegister.text.toString().trim()
+                )
+                val password = edPasswordRegister.text.toString()
+                viewModel.createAccountWithEmailAndPassword(user, password)
+            }
+        }
+
+        //lifecycleScope.launchWhenStarted {  }
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.register.collect {
+                    when(it) {
+                        is Resource.Loading -> {
+                            binding.buttonRegisterRegister.startAnimation()
+                        }
+
+                        is Resource.Success -> {
+                            Log.d("test", it.data.toString())
+                            binding.buttonRegisterRegister.revertAnimation()
+                        }
+
+                        is Resource.Error -> {
+                            Log.e(TAG, it.message.toString())
+                            binding.buttonRegisterRegister.revertAnimation()
+                        }
+                        else -> Unit
+                    }
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/furnitureapp/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/example/furnitureapp/viewmodel/RegisterViewModel.kt
@@ -8,20 +8,22 @@ import com.google.firebase.auth.FirebaseUser
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
 
 @HiltViewModel
-class LoginRegisterViewModel @Inject constructor(
+class RegisterViewModel @Inject constructor(
     private val firebaseAuth: FirebaseAuth
 ) : ViewModel() {
 
-    private val _register = MutableStateFlow<Resource<FirebaseUser>>(Resource.Loading())
+    private val _register = MutableStateFlow<Resource<FirebaseUser>>(Resource.Unspecified())
     var register: Flow<Resource<FirebaseUser>> = _register
 
     fun createAccountWithEmailAndPassword(user: User, password: String) {
+        runBlocking {
+            _register.emit(Resource.Loading())
+        }
         firebaseAuth.createUserWithEmailAndPassword(user.email, password)
             .addOnSuccessListener {
                 it.user?.let {
