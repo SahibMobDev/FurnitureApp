54be679f75bc713b0052bed4179d41802c6d1d6b
SahibMobDev <sahibmobdev@gmail.com>
2023-07-16 13:47:14 +0400

    Introduction viewModel.

--- app/src/main/java/com/example/furnitureapp/fragments/loginRegister/IntroductionFragment.kt
+++ app/src/main/java/com/example/furnitureapp/fragments/loginRegister/IntroductionFragment.kt
@@ -1,16 +1,31 @@
 package com.example.furnitureapp.fragments.loginRegister
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.example.furnitureapp.R
+import com.example.furnitureapp.activities.ShoppingActivity
 import com.example.furnitureapp.databinding.FragmentIntroductionBinding
+import com.example.furnitureapp.viewmodel.IntroductionViewModel
+import com.example.furnitureapp.viewmodel.IntroductionViewModel.Companion.ACCOUNT_OPTIONS_FRAGMENT
+import com.example.furnitureapp.viewmodel.IntroductionViewModel.Companion.SHOPPING_ACTIVITY
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class IntroductionFragment : Fragment(R.layout.fragment_introduction) {
     private lateinit var binding: FragmentIntroductionBinding
+
+    private val viewModel by viewModels<IntroductionViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,7 +39,28 @@ class IntroductionFragment : Fragment(R.layout.fragment_introduction) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.navigate.collect {
+                    when(it) {
+                        SHOPPING_ACTIVITY -> {
+                            Intent(requireActivity(), ShoppingActivity::class.java).also { intent ->
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                                startActivity(intent)
+                        }
+                    }
+                        ACCOUNT_OPTIONS_FRAGMENT -> {
+                            findNavController().navigate(it)
+                        }
+
+                        else -> Unit
+                    }
+                }
+            }
+        }
+
         binding.buttonStart.setOnClickListener {
+            viewModel.startButtonClick()
             findNavController().navigate(R.id.action_introductionFragment_to_accountOptionsFragment)
         }
     }
